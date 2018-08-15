00fb5cfa3e649a1c0ac116dbcfd381356df752d0
Dronan13 <dronan13@gmail.com>
2018-08-15 15:23:13 -0700

    registr and login, user changed, jwt

---
+++ server/config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    url : 'mongodb://dronan13:18189444XIII@ds217452.mlab.com:17452/mycvapp',
+    secret: 'supersecret'
+  };
--- server/config/db.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    url : 'mongodb://dronan13:18189444XIII@ds217452.mlab.com:17452/mycvapp',
-    dbServer: 'ds217452.mlab.com',
-    dbPort: '17452',
-    dbName: 'mycvapp',
-    dbUser: 'dronan13',
-    dbPass: '18189444XIII'
-  };

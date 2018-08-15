bad6e633333635b62b569d4b7b659c1b5ec16c7e
Dronan13Laptop <dronan13@gmail.com>
2018-08-14 23:20:55 -0700

    some changes

--- server/models/User.js
+++ server/models/User.js
@@ -2,9 +2,32 @@ const mongoose = require('mongoose')
 var Schema = mongoose.Schema
 UserSchema = new mongoose.Schema(
     {
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+          },
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+          },
+        password: {
+            type: String,
+            required: true,
+          },
+        passwordConf: {
+            type: String,
+            required: true,
+          },
+        date: {
+            type: Date,
+            default: Date.now
+        },
         firstname: String,
         lastname: String,
-        email: String,
         phone: String
     }
 );
